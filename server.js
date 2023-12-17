c5ff818c9d1dd9f9389af71007aa8b7c177db18c
SarahSmallwood <133904630+SarahSmallwood@users.noreply.github.com>
2023-12-16 19:07:14 -0500

    bug fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AWS = require('aws-sdk');
+require('dotenv').config(); 
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,13 +10,15 @@ app.use(express.json());
 // Set up AWS SDK with your credentials
 AWS.config.update({
   region: 'US East (Ohio) us-east-2',
+  ARN:"",
+  S3URI:""
 });
 
 const s3 = new AWS.S3();
 const bucketName = 'travelappratw';
 
 // Express route to fetch photo URLs from S3
-app.get('/photos', async (req, res) => {
+app.get('/travelappratwphotos', async (req, res) => {
   try {
     const params = {
       Bucket: bucketName,
@@ -35,3 +38,4 @@ app.get('/photos', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+module.exports = router;
--- src/pages/OurTrips.jsx
+++ src/pages/OurTrips.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import ImageCarousel from '../components/Carousel'
+import React from 'react';
+import ImageCarousel from '../components/Carousel';
+import PhotoGallery from './PhotoGallery';
 
 //divide trips into divs with the compenent as popular travel spots with package prices
 
 
 function OurTrips() {
-    const imageUrls= [
+    const photoUrls= [
       "s3://travelappratw/B0E3D885-E242-4A78-B12C-FA2FE3BB361E 2.jpg",
-      "s3://travelappratw/IMG_0706 2.jpg"
+      "s3://travelappratw/IMG_0706 2.jpg",
+      "s3://travelappratw/IMG_0726.JPG",
     ]
   return (
     <div>OurTrips
-        <ImageCarousel className='tripPictures'images={imageUrls}/>
+        <ImageCarousel className='tripPictures'images={photoUrls}/>
         <div className='ourTrips'>
         <div className='tripOne'></div>
         <div className='tripTwo'></div>
